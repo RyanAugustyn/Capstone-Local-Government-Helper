16a949b10c706fa4a1c0d5ce848228f3e869e35a
Ryan Augustyn <jraugustyn07@gmail.com>
2023-05-07 14:26:12 -0500

    ADD to constituent page list of requests and messages user made

--- frontend/src/pages/ConstituentPage.js
+++ frontend/src/pages/ConstituentPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import useAuth from "../hooks/useAuth";
+import { Link } from "react-router-dom";
 
 const ConstituentPage = () => {
   const { userID } = useParams();
@@ -9,7 +10,35 @@ const ConstituentPage = () => {
   const [user, token] = useAuth();
   const [constituent, setConstituent] = useState({});
   const [blocked, setBlocked] = useState(false);
+  const [userMessages, setUserMessages] = useState([]);
+  const [userRequests, setUserRequests] = useState([]);
 
+  const getMessages = async (constituent) => {
+    try {
+      const response = await axios.get("http://127.0.0.1:5000/api/messages");
+      let filteredData = response.data.filter((message) => {
+        return message.username == constituent.username;
+      });
+
+      setUserMessages(filteredData);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+  const getRequests = async (constituent) => {
+    try {
+      const response = await axios.get("http://127.0.0.1:5000/api/requests");
+      let filteredData = response.data.filter((request) => {
+        return request.requester == constituent.id;
+      });
+
+      setUserRequests(filteredData);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+
+  //get user info and set blocked status
   useEffect(() => {
     const getConstituent = async () => {
       try {
@@ -23,12 +52,13 @@ const ConstituentPage = () => {
         );
         setConstituent(response.data);
         setBlocked(response.data.blocked);
+        getRequests(response.data);
+        getMessages(response.data);
       } catch (error) {
         console.log(error.response.data);
       }
     };
     getConstituent();
-    console.log("constitunet: ", constituent);
   }, []);
 
   //handle click of block button
@@ -86,6 +116,26 @@ const ConstituentPage = () => {
           Block Constituent
         </button>
       </div>
+      <h2>User Requests</h2>
+      {userRequests.map((request, index) => (
+        <Link className="requestLink" to={`/requests/${request.id}`}>
+          <div className="card" key={index}>
+            <div className="card-body">
+              <h5 className="card-title">{request.description}</h5>
+              <p className="card-text">{request.type}</p>
+            </div>
+          </div>
+        </Link>
+      ))}
+      <h2>User Messages</h2>
+      {userMessages.map((message, index) => (
+        <div className="card" key={index}>
+          <div className="card-body">
+            <h5 className="card-title">{message.username}</h5>
+            <p className="card-text">{message.text}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
